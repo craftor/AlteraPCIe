7d4922d6f6f116c544423b1a5bda04c6213b1eb7
Craftor <craftor0924@gmail.com>
2013-03-26 14:47:16 +0800

    update test.c

--- test.c
+++ test.c
@@ -30,23 +30,24 @@ int main()
 	  return 0;
 	}
 
-	for (i=0; i< 10; i++) {
-		printf("Test Time %-6d : ",i);
+	printf("Test Start, Transfer Size = %d KB.\n", BUF_SIZE/1024);
+
+	for (i=0; i< 20; i++) {
+
+		printf("Test No.%-3d | ",i);
 		gettimeofday (&tv, NULL);
 		start_time = tv.tv_usec;
 		write(fd,buf,sizeof(buf));
 		gettimeofday (&tv, NULL);
 		stop_time = tv.tv_usec;
 
-		//printf("stop time is %lld\n",stop_time);
-		//printf("start time is %lld\n",start_time);
-
+		printf("t=%lld us , ",stop_time - start_time);
 		/**
 		 * delta_time = stop_time - start_time;
 		 * speed = (BUF_SIZE/(1024*1024))/(delta_time*1000*1000);
 		 */
 		speed = BUF_SIZE*0.9536/(stop_time - start_time);
-		printf("Write Speed = %-8.2f MB/s | ",speed);
+		printf("WR = %-4.2f MB/s | ",speed);
 
 		offset = lseek(fd, 0, SEEK_SET);
 		gettimeofday(&tv, NULL);
@@ -55,10 +56,9 @@ int main()
 		gettimeofday(&tv, NULL);
 		stop_time = tv.tv_usec;
 
-		//printf("stop time is %lld\n",stop_time);
-		//printf("start time is %lld\n",start_time);
+		printf("t=%lld us , ", stop_time - start_time);
 		speed = BUF_SIZE*0.9536/(stop_time - start_time);
-		printf("Read Speed = %-8.2f MB/s \n",speed);
+		printf("RD = %-4.2f MB/s \n",speed);
 
 		usleep(1000*100);
 	}
